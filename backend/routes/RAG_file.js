a50a420f8ce771f500f7588b409327e50dcc2c12
eliasmaral <eliasseverianoamaral@gmail.com>
2025-05-04 01:44:32 -0300

    implementação de loaders para arquivos PDF e TXT

--- backend/routes/RAG_file.js
+++ backend/routes/RAG_file.js
@@ -2,13 +2,12 @@ import express from "express";
 import { RAGFileController } from "../controllers/RAG_file_controllers.js";
 import multer from "multer";
 
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({
+	storage: multer.memoryStorage(),
+});
 const router = express.Router();
 
 router.post("/upload", upload.single("file"), RAGFileController.newRAGFile);
 router.post("/chat", RAGFileController.query);
-
-
-
 
 export default router;
--- backend/services/RAG_file_service.js
+++ backend/services/RAG_file_service.js
@@ -5,14 +5,16 @@ import { QdrantVectorStore } from "@langchain/qdrant";
 import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
 import { pull } from "langchain/hub";
 import { Annotation, StateGraph } from "@langchain/langgraph";
+import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+import { TextLoader } from "langchain/document_loaders/fs/text";
 
 const EMBED_MODEL = process.env.OPENIA_EMBED_MODEL;
 const CHAT_MODEL = process.env.OPENIA_CHAT_MODEL;
 const OPENAI_AI = process.env.OPENAI_API_KEY;
 
 const embeddings = new OpenAIEmbeddings({
-	apiKey: OPENAI_AI, 
-	batchSize: 2048, 
+	apiKey: OPENAI_AI,
+	batchSize: 2048,
 	model: EMBED_MODEL,
 });
 const llm = new ChatOpenAI({
@@ -45,7 +47,7 @@ const StateAnnotation = Annotation.Root({
 });
 
 const splitter = new RecursiveCharacterTextSplitter({
-	chunkSize: 1000,
+	chunkSize: 5000,
 	chunkOverlap: 200,
 	separators: ["\n\n", "\n"],
 });
@@ -76,22 +78,49 @@ const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
 	collectionName: COLLECTION_NAME,
 });
 
+async function createBlob(buffer, type) {
+	return new Blob([buffer], { type });
+}
+
+async function loadPDFDocument(blob) {
+	const loader = new WebPDFLoader(blob, {
+		splitPages: false,
+	});
+	return loader.load();
+}
+
+async function loadTextDocument(blob) {
+	const loader = new TextLoader(blob);
+	return loader.load();
+}
+
 export const RAGFileService = {
 	newFile: async ({ group, name, content }) => {
-		if (!content || !content.length) {
-			throw new Error("Conteúdo do arquivo e nome são obrigatórios.");
-		}
-
 		await ensureCollectionExists();
 
 		try {
+			let doc;
+			const blob = await createBlob(content.buffer, content.mimetype);
+
+			switch (content.mimetype) {
+				case "application/pdf":
+					doc = await loadPDFDocument(blob);
+					break;
+
+				case "text/plain":
+					doc = await loadTextDocument(blob);
+					break;
+
+				default:
+					throw new Error(`Tipo de arquivo não suportado: ${content.mimetype}`);
+			}
+
 			const allSplits = await splitter.splitDocuments([
 				new Document({
-					pageContent: content,
+					pageContent: doc[0].pageContent,
 					metadata: { group, name, createdAt: new Date() },
 				}),
 			]);
-
 			await vectorStore.addDocuments(allSplits);
 			return vectorStore;
 		} catch (error) {
