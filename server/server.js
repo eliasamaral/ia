d41a70c3108fef0e39595ea823f5e1ced906921c
Elias Amaral <eliasseverianoamaral@gmail.com>
2025-07-27 15:01:05 -0300

    remove serviços docker para app e server

--- server/server.js
+++ server/server.js
@@ -3,19 +3,19 @@ import cors from "cors";
 import RAGRoutes from "./routes/RAG_file.js";
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(
 	cors({
-		origin: "http://frontend:8080",
+		origin: "http://app:8080",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"]
 	}),
 );
-app.get("/", async (req, res) => {
-	res.status(500).json({ hello: "word" });
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
 });
 app.use("/api/rag", RAGRoutes);
 
