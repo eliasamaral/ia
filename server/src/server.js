0d3193b18d7fe216db704ddf1ae073d209eadaef
Elias Amaral <eliasseverianoamaral@gmail.com>
2025-07-31 23:14:32 -0300

    atualização do cors

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ const PORT = 3001;
 app.use(express.json());
 app.use(
 	cors({
-		origin: "*",
+		origin: ["http://localhost:5173"],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 	}),
